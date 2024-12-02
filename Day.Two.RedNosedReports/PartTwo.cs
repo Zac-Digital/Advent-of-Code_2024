bc4d15d81d19933a3f7c16c775737c20e1eba739
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2024-12-02 23:15:14 +0000

    feat: day 2 part 2

--- Day.Two.RedNosedReports/PartTwo.cs
+++ Day.Two.RedNosedReports/PartTwo.cs
@@ -2,7 +2,31 @@ namespace Day.Two.RedNosedReports;
 
 public static class PartTwo
 {
+    private static bool IsIncreasing(List<int> report) => report.SequenceEqual(report.Order());
+    private static bool IsDecreasing(List<int> report) => report.SequenceEqual(report.OrderDescending());
+
+    private static bool IsOrdered(List<int> report) => IsIncreasing(report) || IsDecreasing(report);
+
+    private static bool IsInRange(List<int> report)
+    {
+        for (int i = 0; i < report.Count - 1; i++)
+        {
+            if (Math.Abs(report[i] - report[i + 1]) is < 1 or > 3) return false;
+        }
+
+        return true;
+    }
+
     public static void Run()
     {
+        // Sorry.. Felt like doing something horribly cursed :)
+        Console.WriteLine(
+            $"Day 2, Part 2 : {Puzzle.Parse()
+                .Count(report =>
+                    report.Where((_, i) =>
+                    {
+                        List<int> reportWithSkippedIndex = report.Where((_, j) => i != j).ToList();
+                        return IsOrdered(reportWithSkippedIndex) && IsInRange(reportWithSkippedIndex);
+                    }).Any())}");
     }
 }
