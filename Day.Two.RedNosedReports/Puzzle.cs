054d60bab4f9a09802bc4a475035eb36d153818f
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2024-12-02 22:00:37 +0000

    refactor: day 2 part 1

--- Day.Two.RedNosedReports/Puzzle.cs
+++ Day.Two.RedNosedReports/Puzzle.cs
@@ -4,6 +4,6 @@ internal static class Puzzle
 {
     private const string Input = "";
 
-    internal static int[][] Parse() => Input.Split(Environment.NewLine)
-        .Select(report => report.Split(" ").Select(int.Parse).ToArray()).ToArray();
+    internal static List<List<int>> Parse() => Input.Split(Environment.NewLine)
+        .Select(report => report.Split(" ").Select(int.Parse).ToList()).ToList();
 }
