c7f64c7c0aa1a2faeea4d93625d614a1fe3cf8a7
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2024-12-03 21:24:43 +0000

    feat: day 3 part 2

--- Day.Three.MullItOver/PartTwo.cs
+++ Day.Three.MullItOver/PartTwo.cs
@@ -8,6 +8,32 @@ public class PartTwo : Compute
 
     protected override int Run()
     {
-        return 0;
+        int result = 0;
+
+        bool execute = true;
+
+        foreach (string instruction in Puzzle.ParsePartTwo())
+        {
+            switch (instruction)
+            {
+                case "do()":
+                    execute = true;
+                    break;
+                case "don't()":
+                    execute = false;
+                    break;
+                default:
+                    if (execute)
+                    {
+                        string[] splitComma = instruction.Split(',');
+                        int nLeft = int.Parse(splitComma[0].Split('(')[1]);
+                        int nRight = int.Parse(splitComma[1].Split(')')[0]);
+                        result += nLeft * nRight;
+                    }
+                    break;
+            }
+        }
+
+        return result;
     }
 }
--- Day.Three.MullItOver/Puzzle.cs
+++ Day.Three.MullItOver/Puzzle.cs
@@ -6,9 +6,15 @@ internal static partial class Puzzle
 {
     private const string Input = "";
 
-    internal static List<string> Parse() =>
-        MulExpression().Matches(Input).Select(x => x.Value).ToList();
+    internal static List<string> ParsePartOne() =>
+        MulExpressionPartOne().Matches(Input).Select(x => x.Value).ToList();
+
+    internal static List<string> ParsePartTwo() =>
+        MulExpressionPartTwo().Matches(Input).Select(x => x.Value).ToList();
 
     [GeneratedRegex(@"mul\(\d{1,3},\d{1,3}\)")]
-    private static partial Regex MulExpression();
+    private static partial Regex MulExpressionPartOne();
+
+    [GeneratedRegex(@"(do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\))")]
+    private static partial Regex MulExpressionPartTwo();
 }
