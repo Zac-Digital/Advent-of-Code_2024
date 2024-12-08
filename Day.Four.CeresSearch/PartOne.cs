4f73a3ebe556fa609e9d8a21dbadf682d0c0458e
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2024-12-08 12:17:09 +0000

    feat: day 4 part 1

---
+++ Day.Four.CeresSearch/PartOne.cs
@@ -0,0 +1,93 @@
+using Shared.Code;
+
+namespace Day.Four.CeresSearch;
+
+public class PartOne : Compute
+{
+    public PartOne() : base("Day 4, Part 1") { }
+
+    private static int Walk(string[] board, int row, int column)
+    {
+        if (board[row][column] != 'X') return 0;
+
+        int rows = board.Length;
+        int columns = board[0].Length;
+
+        int count = 0;
+
+        // North
+        if (row - 3 >= 0 && board[row - 1][column] == 'M' && board[row - 2][column] == 'A' &&
+            board[row - 3][column] == 'S')
+        {
+            count++;
+        }
+
+        // East
+        if (column + 3 < columns && board[row][column + 1] == 'M' && board[row][column + 2] == 'A' &&
+            board[row][column + 3] == 'S')
+        {
+            count++;
+        }
+
+        // South
+        if (row + 3 < rows && board[row + 1][column] == 'M' && board[row + 2][column] == 'A' &&
+            board[row + 3][column] == 'S')
+        {
+            count++;
+        }
+
+        // West
+        if (column - 3 >= 0 && board[row][column - 1] == 'M' && board[row][column - 2] == 'A' &&
+            board[row][column - 3] == 'S')
+        {
+            count++;
+        }
+
+        // North-East
+        if (row - 3 >= 0 && column + 3 < columns && board[row - 1][column + 1] == 'M' &&
+            board[row - 2][column + 2] == 'A' && board[row - 3][column + 3] == 'S')
+        {
+            count++;
+        }
+
+        // South-East
+        if (row + 3 < rows && column + 3 < columns && board[row + 1][column + 1] == 'M' &&
+            board[row + 2][column + 2] == 'A' && board[row + 3][column + 3] == 'S')
+        {
+            count++;
+        }
+
+        // North-West
+        if (row - 3 >= 0 && column - 3 >= 0 && board[row - 1][column - 1] == 'M' && board[row - 2][column - 2] == 'A' &&
+            board[row - 3][column - 3] == 'S')
+        {
+            count++;
+        }
+
+        // South-West
+        if (row + 3 < rows && column - 3 >= 0 && board[row + 1][column - 1] == 'M' &&
+            board[row + 2][column - 2] == 'A' && board[row + 3][column - 3] == 'S')
+        {
+            count++;
+        }
+
+        return count;
+    }
+
+    protected override int Run()
+    {
+        int result = 0;
+
+        string[] board = Puzzle.Parse();
+
+        for (int row = 0; row < board.Length; row++)
+        {
+            for (int column = 0; column < board[0].Length; column++)
+            {
+                result += Walk(board, row, column);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ Day.Four.CeresSearch/Puzzle.cs
@@ -0,0 +1,10 @@
+using System.Text.RegularExpressions;
+
+namespace Day.Four.CeresSearch;
+
+internal static partial class Puzzle
+{
+    private const string Input = "";
+
+    internal static string[] Parse() => Input.Split(Environment.NewLine);
+}
